a279fd4aba13b8d0850d1eda0a1051ab87a366f1
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 20:42:15 -0300

    Exe1.py

---
+++ lista/Exe1.py
@@ -0,0 +1,7 @@
+# Escreva um programa que peça um número inteiro positivo ao usuário e exiba todos os
+# números de 1 até esse número, um por linha.
+
+numero=int(input("Digite um número inteiro: "))
+
+for i in range(numero):
+    print(i+1)
